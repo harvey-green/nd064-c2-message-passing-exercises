9f4900e222446f51db8519b0db3f4dd75337ced7
Harvey Green <greenh@iinet.net.au>
2021-12-13 20:36:23 +0800

    lesson 3 rest and flask exercise.

--- lesson-3-implementing-message-passing/flask-demo/app.py
+++ lesson-3-implementing-message-passing/flask-demo/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from .services import retrieve_pet, create_pet
+from .services import retrieve_orders, create_order
 
 app = Flask(__name__)
 
@@ -9,29 +9,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/demo/<path_demo>', methods=['POST'])
-def demo(path_demo=None):
-    # Showcase different ways in which data can be passed
-    path_value = path_demo
-    header_value = request.headers.get('header_demo', None)
-    param_value = request.args.get('param_demo', None)
-    body_value = request.data
-    return {
-        'result': {
-            'path': path_value,
-            'header': header_value,
-            'param': param_value,
-            'body': str(body_value),
-        }
-    }
-
-@app.route('/api/items/<item_id>', methods=['GET', 'POST'])
-def pets(item_id):
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computers():
     if request.method == 'GET':
-        return Response(json.dumps(retrieve_item(pet_id)), 200, {'Content-Type': 'application/json'}
+        return jsonify(retrieve_orders())
     elif request.method == 'POST':
         request_body = request.json
-        return Response(json.dumps(create_item(item_id, request_body)
+        return jsonify(create_order(request_body))
     else:
         raise Exception('Unsupported HTTP request type.')
 
--- lesson-3-implementing-message-passing/flask-demo/services.py
+++ lesson-3-implementing-message-passing/flask-demo/services.py
@@ -1,22 +1,32 @@
-def retrieve_item(item_id):
+from .enums import Status
+
+def retrieve_orders():
     """
-    This is a stubbed method of retrieving a resource. It doesn't actually do anything.
+    This is a stubbed method of retrieving multiple resources. It doesn't do anything.
     """
-    return {
-        "id": item_id,
-        "brand_name": "Clean Breathing",
-        "name": "Air Purifier",
-        "weight": 12.3,
-    }
+    return [
+        {
+            "order_id": 1,
+            "created_by": "justin",
+            "status": Status.Completed.value,
+            "created_at": "2020-09-28T08:56:44",
+            "equipment": [
+                "KEYBOARD"
+            ]
+        },
+        {
+            "order_id": 2,
+            "created_by": "tom",
+            "status": Status.Queued.value,
+            "created_at": "2020-09-28T09:56:44",
+            "equipment": [
+               "MOUSE",
+               "WEBCAM"
+            ]
+        }
+    ]
 
 
-def create_item(item_id):
-    """
-    This is a stubbed method of creating a resource. It doesn't actually do anything.
-    """
-    return {
-        "id": item_id,
-        "brand_name": "Clean Breathing",
-        "name": "Air Purifier",
-        "weight": 12.3,
-    }
+def create_order(body):
+    # do stuff to process the data
+    return body
